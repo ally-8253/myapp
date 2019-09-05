38018681ccf8e551d2210422ec638fc26590bbd5
Dobby <ally8253@gmail.com>
2019-09-05 17:14:23 +0600

    add refs to name and phone input

--- src/Components/AddContact.js
+++ src/Components/AddContact.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './AddContact.css';
+import { declareExportAllDeclaration } from '@babel/types';
 
 function AddContact() {
+    let newContactName = React.createRef();
+    let newContactPhone = React.createRef();
+
+    let addContact = () => {
+        let name = newContactName.current.value;
+        let phone = newContactPhone.current.value;
+        alert(name + phone);
+    };
+
     return (
         <div className="add-contact">
             <div className="container">
                 <form>
-                    <input id="name" placeholder="Enter name of contact" />
-                    <input id="phone" placeholder="Enter phone number of contact" />
+                    <input ref={newContactName} id="name" placeholder="Enter name of contact" />
+                    <input ref={newContactPhone} id="phone" placeholder="Enter phone number of contact" />
                 </form>
-                <button className="add-contact-btn">Add contact</button>
+                <button onClick={addContact} className="add-contact-btn">Add contact</button>
             </div>
         </div>
     );
--- src/Components/ContactLine.js
+++ src/Components/ContactLine.js
@@ -3,13 +3,16 @@ import './ContactLine.css';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 function ContactLine(props) {
+    let deleteContact = () => {
+       alert('This button will delete the contact');
+    };
     return (
         <div className="contact-line">
             <p>{props.name}</p>
             <p>{props.phone}</p>
             <div className="btns">
                 <NavLink to="/edit-contact">Edit</NavLink>
-                <a src="#">Delete</a>
+                <a onClick={deleteContact} className="delete-btn">Delete</a>
             </div>
         </div>
     );
